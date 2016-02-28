ddeb7c08ace724367b1b04738e727b41cdf75996
bronzdoc <lsagastume1990@gmail.com>
2016-02-27 23:31:50 -0600

    Environment variables will be a pain in the ass! use yaml file for credentials instead

--- packtgrab.rb
+++ packtgrab.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require "bundler/inline"
+require  "yaml"
 
 LOG_FILE = "packtgrab.log"
 
@@ -8,7 +9,7 @@ gemfile true do
   source "https://rubygems.org"
   gem "json"
   gem "mechanize", "~> 2.7"
-  gem 'colored',   "~> 1.2"
+  gem "colored",   "~> 1.2"
 end
 
 agent = Mechanize.new do |config|
@@ -16,12 +17,15 @@ agent = Mechanize.new do |config|
   config.user_agent_alias = 'Windows Chrome' #:troll:
 end
 
+# Get credential
+auth = YAML.load_file("packt_auth.yaml")
+
 puts "packt grab started.....".cyan
 agent.get("https://www.packtpub.com/packt/offers/free-learning") do |promo_page|
   # Get login form and sumbit
   page = promo_page.form_with(dom_id: "packt-user-login-form") do |form|
-    form.email         = ENV["EMAIL"]
-    form.password      = ENV["PASSWORD"]
+    form.email         = auth["PACKT_EMAIL"]
+    form.password      = auth["PACKT_PASSWORD"]
     form.form_id       = form["form_id"]
     form.form_build_id = form["form_build_id"]
   end.submit
